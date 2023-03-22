d65d4767e65fb2dc2d4318af4b19cadd56072f37
myname <youssef@gmail.com>
2023-03-22 08:56:24 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,8 @@
-int _putchar(char);
+#ifndef main_h
+	#define main_h
+
+
+	int _putchar(char);
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
@@ -10,4 +14,8 @@ int _putchar(char);
 	void times_table(void);
 	int add(int, int);
 	void print_to_98(int n);
+	void print_times_table(int n);
+
+
+	#endif
 
